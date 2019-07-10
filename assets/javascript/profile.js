dfc6a6386bb1a996bc94465d6863ee98f5c97a37
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-07-09 18:26:11 -0700

    added the itunes link

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -9,3 +9,21 @@ function getArtistToQuery() {
 
   return currentArtist;
 }
+
+var currentArtist = getArtistToQuery();
+$('.artName').text(currentArtist);
+
+axios({
+    url: `https://itunes.apple.com/search?term=${currentArtist}&limit=30`,
+    method: "GET"
+  })
+    .then(function(response) {
+      console.log(response.data.results[0].artistViewUrl);  
+      var tempStore = response.data.results[0].artistViewUrl;
+      $('.iLink').attr('href',tempStore);
+      $('.iLink').attr('target','_blank');
+
+    })   
+    .catch(function(err) {
+      console.error(err);
+    });
